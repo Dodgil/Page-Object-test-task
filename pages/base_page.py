8a812da1100423edd58f08cbf6714740e2b37c0f
i.baranov <ILYA-669@yandex.ru>
2021-03-31 08:55:32 +0300

    Added Page Object files

---
+++ pages/base_page.py
@@ -0,0 +1,8 @@
+"""Базовая страница для проекта: BasePage"""
+class BasePage():
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+
+    def open(self): 
+        self.browser.get(self.url)
